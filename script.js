8601a5a9bb9c877324cff075787b8e427a5045b3
Noddy7866 <knoddy782@gmail.com>
2026-08-25 22:40:00 +0530

    Update ADMIN_PASS to a new value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DEFAULT_UPI="noddyk782@okicici";
 const DEFAULT_GOAL=10000;
-const ADMIN_PASS="notnoddylive@1989";
+const ADMIN_PASS="notnoddylive@12345";
 let selectedAmount=0;
 
 function getSettings(){
